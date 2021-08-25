ea0ebfa481caa29f0b06b049182b397da309197d
UsmanQasim <1998usman@gmail.com>
2021-08-26 03:04:04 +0500

    Styling Done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import App from "./App.js";
-import './index.scss';
-import './colors.scss';
+import "./index.scss";
+import "./colors.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
--- src/pages/Panel/components/dashboard/Dashboard.js
+++ src/pages/Panel/components/dashboard/Dashboard.js
@@ -1,30 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./dashboard.module.scss";
+import { CgNotes } from "react-icons/cg";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const Dashboard = ({ inquiries }) => {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <div>
-      {inquiries.map((inquiry, key) => (
-        <p key={key}>
-          <table>
-            <tr>
-              <th>First</th>
-              <th>Second</th>
-              <th>Third</th>
-            </tr>
-            <tr>
+    <div className={styles.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Action</th>
+            <th>Response</th>
+            <th>Customer Information</th>
+            <th>Event Details</th>
+            <th>Booking Details</th>
+          </tr>
+        </thead>
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+        <tbody>
+          {inquiries.map((inquiry, key) => (
+            <tr key={key}>
+              {/* Action */}
               <td>
-                <div>
-                  {inquiry.gender}
-                  {inquiry.firstName}
-                </div>
-                <div>
-                  <td>{inquiry.lastName}</td>
+                <div style={{ textAlign: "center" }}>
+                  <button onClick={() => setModalShow(true)}>
+                    <CgNotes size="25px" />
+                  </button>
                 </div>
               </td>
-              <td>{inquiry.packages}</td>
+              {/* Response */}
+              <td>
+                <div>
+                  <label>RESPONSE :</label>
+                </div>
+                <div>
+                  <label>Sales Customer :</label>
+                </div>
+              </td>
+              {/* customer details */}
+              <td>
+                <div>
+                  <label>Name :</label>
+                  {inquiry.gender}
+                  {inquiry.firstName}
+                  {inquiry.lastName}
+                </div>
+                <div>
+                  <label>Email :</label>
+                  {inquiry.email}
+                </div>
+                <div>
+                  <label>Postal Code :</label>
+                  {inquiry.pcode}
+                </div>
+                <div>
+                  <label>Phone NO :</label>
+                  {inquiry.phone_no}
+                </div>
+              </td>
+              {/* Event Details */}
+              <td>
+                <div>
+                  <label>Looking For :</label>
+                  {inquiry.packages}
+                </div>
+                <div>
+                  <label>DATE :</label>
+                  {inquiry.date}
+                </div>
+              </td>
+              {/* Booking Details */}
+              <td>
+                <div>
+                  <label>Venue</label>
+                  {inquiry.venue_to_be}
+                </div>
+                <div>
+                  <label>Total Guests :</label>
+                  {inquiry.total_guests}
+                </div>
+                <div>
+                  <label>Event Type :</label>
+                  {inquiry.event_type}
+                </div>
+                <div>
+                  <label>Comment:</label>
+                  {inquiry.comment}
+                </div>
+                <div>
+                  <label>How they Know :</label>
+                  {inquiry.how_you_know}
+                </div>
+              </td>
             </tr>
-          </table>
-          {inquiry.firstName}
+          ))}
+        </tbody>
+      </table>
+
+      {/* {inquiry.firstName}
           {inquiry.date}
           {inquiry.lastName}
           {inquiry.gender}
@@ -37,11 +116,31 @@ const Dashboard = ({ inquiries }) => {
           {inquiry.email}
           {inquiry.phone_no}
           {inquiry.comment}
-          {inquiry.how_you_know}
-        </p>
-      ))}
+          {inquiry.how_you_know} */}
     </div>
   );
 };
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">NOTES</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <textarea cols="40" rows="8" style={{ padding: "5px 10px" }} />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={props.onHide}>Update</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default Dashboard;
